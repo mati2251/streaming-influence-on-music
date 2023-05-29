5b06be97a43846117a2e7a58d6fb7f9e2f73b33a
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-05-29 11:29:41 +0200

    [improved] changed detelmiter to semicoln

---
+++ data_extenders/change_detelmiter.py
@@ -0,0 +1,10 @@
+import csv
+## change csv delimiter from ',' to ';'
+file = 'charts_top_10.csv'
+new_file = 'charts_top_10_new.csv'
+with open (file, 'r') as f:
+    csv_reader = csv.reader(f, delimiter=',')
+    with open(new_file, 'w') as new_file:
+        csv_writer = csv.writer(new_file, delimiter=';')
+        for line in csv_reader:
+            csv_writer.writerow(line)
