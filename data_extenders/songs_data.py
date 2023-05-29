0531fd3f9b8598e20dc337fb95eca6e5b467caf7
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-05-29 13:11:54 +0200

    [added] added songs unical for top 10 with audio features

--- data_extenders/songs_data.py
+++ data_extenders/songs_data.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('./charts.csv')
 
-df = df.groupby(['artist', 'song']).agg({'rank': 'min', 'weeks-on-board': 'max'}).reset_index()
+df = df.groupby(['artist', 'song']).agg({'date': 'first','rank': 'min', 'weeks-on-board': 'max'}).reset_index()
 print(df)
 df.to_csv('./songs.csv', index=False)
